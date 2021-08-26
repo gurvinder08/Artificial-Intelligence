ac1e6f6444193dd20874545b025c8222e2f030dd
Gurvinder Kaur <77490072+gurvinder08@users.noreply.github.com>
2021-08-26 18:11:03 +0530

    Create Data Structures.py

---
+++ Data Structures.py
@@ -0,0 +1,14 @@
+E={8,0,6,2,4}
+N={1,2,3,4,5}
+#Union of E and N
+U=E.union(N)
+print(U)
+# Intersection
+I=E.intersection(N)
+print(I)
+#Difference
+D=E.difference(N)
+print(D)
+#Symmetric Difference
+S=E.symmetric_difference(N)
+print(S)
